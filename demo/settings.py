ed1421282ab3214cac6427ad3898c843854f291f
ToniMCano <toni.mrtnz.cn@gmail.com>
2024-09-11 14:11:52 +0200

    Desarrollo Web

--- demo/settings.py
+++ demo/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'demo.wsgi.application' # !!! en el servidor estaba 'core.wsg
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
         'default': {
                   'ENGINE': 'django.db.backends.mysql',
                    'NAME': 'toni_test',
@@ -85,6 +85,14 @@ DATABASES = {
                    'PORT': '',
          }
   } 
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
 
 
 # Password validation
